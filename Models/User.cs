8b0b6c8f526be5a1c84f2b32c43c5a237aa6bbe1
justcvb <justcvb@gmail.com>
2020-05-28 15:12:56 +0800

    Add User class

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientBase
+{
+    class User : BaseModel
+    {
+        public User() { }
+
+        public String Login { get; set; }
+        public String Password { get; set; }
+    }
+}
