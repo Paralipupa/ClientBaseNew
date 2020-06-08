6aba0ef4890f57be94b86093581845f38dc85c74
justcvb <justcvb@gmail.com>
2020-06-08 23:24:52 +0800

    Add NewLineRemoveConverter

---
+++ Converters/NewLineRemoveConverter.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ClientBase
+{
+    class NewLineRemoveConverter : IValueConverter
+    {
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            if (value is String s)
+            {
+                return s.Replace("\n", "").Replace("\r", "");
+            }
+            return value;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
