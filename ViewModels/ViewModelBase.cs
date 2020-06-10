c6eb239a55c8715654db7bd69d25e8514e63eefc
justcvb <justcvb@gmail.com>
2020-06-11 01:04:01 +0800

    Add Set method to base classes of models and viewmodels

--- ViewModels/ViewModelBase.cs
+++ ViewModels/ViewModelBase.cs
@@ -17,5 +17,15 @@ namespace ClientBase
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
         }
+
+        public bool Set<T>(ref T field, T value, [CallerMemberName] String property = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            field = value;
+            return true;
+        }
     }
 }
